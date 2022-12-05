1bc207ff775afd0f2838fc9fbe1d9d76ad925264
MrDos180 <ilya180@yandex.ru>
2022-12-05 19:57:57 +0300

    first commit

---
+++ src/main/java/TestList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+
+
+public class CollectionList {
+
+    public void arrayListAddString(ArrayList<String> list, String element) {
+        list.add(element);
+        System.out.println("Элемент "+element+" добавлен в список");
+    }
+
+    public void arrayListFindElement(ArrayList<String> list, String element) {
+        for (String s : list) {
+            if (s.equals(element)) {
+                System.out.println("Элемент " + element + " найден в списке");
+            }
+        }
+    }
+
+    public void arrayListDelete(ArrayList<String> list, String element) {
+        list.remove(element);
+        System.out.println("элемент " + element+" удален из списка");
+    }
+
+    public void printArrayListAllElements(ArrayList<String> list){
+        for (String s : list){
+            System.out.println(s);
+        }
+    }
+
+
+}
